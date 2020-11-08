442fd75e8e1585c1ae75de7b5b3fd0027ea75b7a
Nyirom2 <nyirocsabi@gmail.com>
2020-11-08 09:12:08 +0100

    Minden feladat kész az elfordítás (new layout)-on kívül.

--- app/src/main/java/nyiro/csaba/akasztofa/MainActivity.java
+++ app/src/main/java/nyiro/csaba/akasztofa/MainActivity.java
@@ -1,13 +1,16 @@
 package nyiro.csaba.akasztofa;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -28,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Character> tippeltKarakterek = new ArrayList<Character>();
     private String eddigKitalalt;
     private int probalkozasok;
+    private AlertDialog alertDialog;
+    private AlertDialog.Builder builder;
+    private String[] szavakArray;
+    private char[] karakterekArray;
+    private Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,14 +56,14 @@ public class MainActivity extends AppCompatActivity {
 
         // SZAVAK
         Random r = new Random();
-        randomSzoIndex = r.nextInt(10);
-        final String[] szavakArray = {"Android", "Flutter", "Akasztófa", "Programozás", "Mobil", "Java", "Kotlin", "Dart", "Alkalmazás", "Programnyelv"};
+        randomSzoIndex = r.nextInt(11);
+        szavakArray = new String[]{"Android", "Flutter", "Akasztófa", "Programozás", "Mobil", "Java", "Kotlin", "Dart", "Alkalmazás", "Programnyelv"};
         eddigKitalalt = szavakArray[randomSzoIndex].replaceAll(".", "_ ");
         szo.setText(eddigKitalalt);
 
         // KARAKTEREK
         karakterIndex = 0;
-        final char[] karakterekArray = {'A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ő', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'W', 'X', 'Y', 'Z'};
+        karakterekArray = new char[]{'A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ő', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'W', 'X', 'Y', 'Z'};
         tippBetu.setText(String.valueOf(karakterekArray[karakterIndex]));
 
         // ESEMÉNYEK
@@ -102,72 +110,113 @@ public class MainActivity extends AppCompatActivity {
         Btn_Tipp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tippeltKarakterek.add(karakterekArray[karakterIndex]);
-                eddigKitalalt = szavakArray[randomSzoIndex].toUpperCase();
-                for(char karakter: karakterekArray) {
-                    if(!tippeltKarakterek.contains(karakter)) {
-                        eddigKitalalt = eddigKitalalt.replaceAll(String.valueOf(karakter), "_");
+                if(!tippeltKarakterek.contains(karakterekArray[karakterIndex])) {
+                    tippeltKarakterek.add(karakterekArray[karakterIndex]);
+                    eddigKitalalt = szavakArray[randomSzoIndex].toUpperCase();
+                    for(char karakter: karakterekArray) {
+                        if(!tippeltKarakterek.contains(karakter)) {
+                            eddigKitalalt = eddigKitalalt.replaceAll(String.valueOf(karakter), "_");
+                        }
                     }
-                }
-                if(eddigKitalalt.contains(String.valueOf(karakterekArray[karakterIndex]))) {
-                    // TALÁLT EGY OLYAT AMI BENNE VAN
-                    if(!eddigKitalalt.contains("_")) {
-                        // MINDEN BETŰT KITALÁLT A FELHASZNÁLÓ - JÁTÉK VÉGE
-                    };
+                    if(eddigKitalalt.contains(String.valueOf(karakterekArray[karakterIndex]))) {
+                        // TALÁLT EGY OLYAT AMI BENNE VAN
+                        Toast.makeText(MainActivity.this,"Jó tipp", Toast.LENGTH_SHORT).show();
+                        if(!eddigKitalalt.contains("_")) {
+                            // MINDEN BETŰT KITALÁLT A FELHASZNÁLÓ - JÁTÉK VÉGE
+                            alertDialog = setUzenet("Helyes megfejtés!");
+                            alertDialog.show();
+                        };
+                    } else {
+                        // AKASZTJA AZ EMBERT
+                        Toast.makeText(MainActivity.this,"Rossz tipp", Toast.LENGTH_SHORT).show();
+                        probalkozasok--;
+                        switch(probalkozasok) {
+                            case 13:
+                                akasztofa.setImageResource(R.drawable.akasztofa13);
+                                break;
+                            case 12:
+                                akasztofa.setImageResource(R.drawable.akasztofa12);
+                                break;
+                            case 11:
+                                akasztofa.setImageResource(R.drawable.akasztofa11);
+                                break;
+                            case 10:
+                                akasztofa.setImageResource(R.drawable.akasztofa10);
+                                break;
+                            case 9:
+                                akasztofa.setImageResource(R.drawable.akasztofa09);
+                                break;
+                            case 8:
+                                akasztofa.setImageResource(R.drawable.akasztofa08);
+                                break;
+                            case 7:
+                                akasztofa.setImageResource(R.drawable.akasztofa07);
+                                break;
+                            case 6:
+                                akasztofa.setImageResource(R.drawable.akasztofa06);
+                                break;
+                            case 5:
+                                akasztofa.setImageResource(R.drawable.akasztofa05);
+                                break;
+                            case 4:
+                                akasztofa.setImageResource(R.drawable.akasztofa04);
+                                break;
+                            case 3:
+                                akasztofa.setImageResource(R.drawable.akasztofa03);
+                                break;
+                            case 2:
+                                akasztofa.setImageResource(R.drawable.akasztofa02);
+                                break;
+                            case 1:
+                                akasztofa.setImageResource(R.drawable.akasztofa01);
+                                break;
+                            case 0:
+                                akasztofa.setImageResource(R.drawable.akasztofa00);
+                                break;
+                        }
+                        if(probalkozasok == 0) {
+                            // VÉGE A JÁTÉKNAK!
+                            alertDialog = setUzenet("Nem sikerült kitalálni!");
+                            alertDialog.show();
+                        }
+                    }
+                    szo.setText(eddigKitalalt.replaceAll(".", "$0 "));
+                    tippBetu.setTextColor(Color.parseColor("#000000"));
                 } else {
-                    // AKASZTJA AZ EMBERT
-                    probalkozasok--;
-                    switch(probalkozasok) {
-                        case 13:
-                            akasztofa.setImageResource(R.drawable.akasztofa13);
-                            break;
-                        case 12:
-                            akasztofa.setImageResource(R.drawable.akasztofa12);
-                            break;
-                        case 11:
-                            akasztofa.setImageResource(R.drawable.akasztofa11);
-                            break;
-                        case 10:
-                            akasztofa.setImageResource(R.drawable.akasztofa10);
-                            break;
-                        case 9:
-                            akasztofa.setImageResource(R.drawable.akasztofa09);
-                            break;
-                        case 8:
-                            akasztofa.setImageResource(R.drawable.akasztofa08);
-                            break;
-                        case 7:
-                            akasztofa.setImageResource(R.drawable.akasztofa07);
-                            break;
-                        case 6:
-                            akasztofa.setImageResource(R.drawable.akasztofa06);
-                            break;
-                        case 5:
-                            akasztofa.setImageResource(R.drawable.akasztofa05);
-                            break;
-                        case 4:
-                            akasztofa.setImageResource(R.drawable.akasztofa04);
-                            break;
-                        case 3:
-                            akasztofa.setImageResource(R.drawable.akasztofa03);
-                            break;
-                        case 2:
-                            akasztofa.setImageResource(R.drawable.akasztofa02);
-                            break;
-                        case 1:
-                            akasztofa.setImageResource(R.drawable.akasztofa01);
-                            break;
-                        case 0:
-                            akasztofa.setImageResource(R.drawable.akasztofa00);
-                            break;
-                    }
-                    if(probalkozasok == 0) {
-                        // VÉGE A JÁTÉKNAK!
-                    }
+                    Toast.makeText(MainActivity.this,"Már tippelted a betűt!", Toast.LENGTH_SHORT).show();
                 }
-                szo.setText(eddigKitalalt.replaceAll(".", "$0 "));
-                tippBetu.setTextColor(Color.parseColor("#000000"));
             }
         });
+    }
+
+    AlertDialog setUzenet(String title) {
+        builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Szeretnél még egyet játszani?")
+                .setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        tippeltKarakterek.clear();
+                        probalkozasok = 13;
+                        Random r = new Random();
+                        randomSzoIndex = r.nextInt(10);
+                        eddigKitalalt = szavakArray[randomSzoIndex].replaceAll(".", "_ ");
+                        szo.setText(eddigKitalalt);
+                        akasztofa.setImageResource(R.drawable.akasztofa13);
+                        karakterIndex = 0;
+                        tippBetu.setText(String.valueOf(karakterekArray[karakterIndex]));
+                        tippBetu.setTextColor(Color.parseColor("#FF0000"));
+                    }
+                })
+                .setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        dialog.cancel();
+                    }
+                })
+                .setTitle(title)
+                .setCancelable(false);
+        alertDialog = builder.create();
+        return alertDialog;
     }
 }
